d4ab5546d248db76c95f9033836f784202f89ac7
Mccoy123 <dominic91@hotmail.co.uk>
2015-02-23 02:36:03 +0000

    add result update 2.1
    managed to get the opponent select to input values from an array list,
    but cannot get it to do this for an array list of users queried from
    parse?

--- www/js/app.js
+++ www/js/app.js
@@ -52,21 +52,128 @@ $(document).ready(function() {
 		alert("input2");
     });
 	
-	//popup funstion
+	//Test Function Delete
+	//popup function 
 	function showPopupText(){
 	var input = document.getElementById("textInput").value;
 	alert("input");
 	}
 	
+	//Test Function Delete
 	function showPopupSelect(){
 	var input2 = document.getElementById("selectInput").value;
 	alert("input");
 	}
 	
-	//addResult
-	//This Currently Works for playerID2 field
+	// Add result functions 3
+	/*$(document).on("pagebeforecreate","#uploadResult",function(){
+		
+		var opponentUsername = Parse.Object.extend("User");
+		var query = new Parse.Query(opponentUsername);
+		query.notEqualTo("objectId", "pHtwexlcv6"); //need to change so the objectId is whoever the current user is
+		query.find({
+			success: function(results) {
+			//do something with this object,,,, list it
+			var select = document.getElementById("selectOpponentPlayer2");
+			  for (var i = 0; i < results.length; i++) {
+				var object = results[i];
+				var el = document.createElement("option");
+				el.textContent = "hi";
+				el.value = "hi";
+				select.appendChild(el);
+				}
+			},
+			error: function(error) {
+				alert("Error: playerId couldnt be collected");
+			}
+		});
+	});*/
+	
+	$(document).on("pagebeforecreate","#uploadResult",function(){
+		//var select = document.getElementById("selectOpponentPlayer2");
+		var options = ["1", "2", "3", "4", "5"];
+		var opponentUsername = Parse.Object.extend("User");
+		var query = new Parse.Query(opponentUsername);
+		query.notEqualTo("objectId", "pHtwexlcv6"); //need to change so the objectId is whoever the current user is
+		//query.find({
+			//success: function(results) {
+				for(var i = 0; i < options.length; i++) {
+					var opt = options[i];
+					var el = document.createElement("option");
+					//el.setAttribute("id", "test44");
+					el.textContent = opt;
+					el.value = opt;
+					var select = document.getElementById("selectOpponentPlayer2");
+					select.appendChild(el);
+					//var test44 = document.getElementById("test44");
+					//alert(test44.value);//option element el is created, why is it not visible
+					//alert(select); //that is evaluated
+				}
+			/*},
+			error: function(error) {
+				alert("Error: playerId couldnt be collected");
+				}
+		});*/
+	});
+	
+	/*$(document).on("pagebeforecreate","#uploadResult",function(){
+		//var select = document.getElementById("selectOpponentPlayer2");
+		var options = ["1", "2", "3", "4", "5"];
+		var opponentUsername = Parse.Object.extend("User");
+		var query = new Parse.Query(opponentUsername);
+		var testArray = new Array();
+		query.notEqualTo("objectId", "pHtwexlcv6"); //need to change so the objectId is whoever the current user is
+		query.find({
+			success: function(results) {
+				
+				for(var i = 0; i < options.length; i++) {
+					var opt = results[i];
+					var playerUserId = opt.get('username');
+					testArray[testArray.length] = playerUserId;
+					alert(testArray[0]);
+					//var el = document.createElement("option");
+					//el.textContent = opt;
+					//el.value = opt;
+					//var select = document.getElementById("selectOpponentPlayer2");
+					//select.appendChild(el);
+					
+				}
+			},
+			error: function(error) {
+				alert("Error: playerId couldnt be collected");
+				}
+		});
+		alert(testArray[0]);
+	});*/
+	
+	//addresult populate opponent 1 //not working just yet
+	/*$(document).on("pagebeforecreate","#uploadResult",function(){
+		//add comment about challenges here
+		var select = document.getElementById("selectOpponentPlayer2");
+		var opponentUsername = Parse.Object.extend("User");
+		var query = new Parse.Query(opponentUsername);
+		query.notEqualTo("objectId", "pHtwexlcv6"); //need to change so the objectId is whoever the current user is
+		query.find({
+			success: function(results) {
+			//do something with this object,,,, list it
+			  for (var i = 0; i < results.length; i++) {
+				var object = results[i];
+				//add select list elements 
+				var playerUserId = object.get('playerID');
+				var z = document.createElement("option");
+				z.textContent = playerUserId;
+				z,value = playerUserId;
+				select.appendChild(z);
+			  }
+			},
+			error: function(error) {
+				alert("Error: playerId couldnt be collected");
+			}
+		});
+	});*/
+	
+	//addResult submit Form 3
 	$('.btn-addResult').click(function(e) {
-	//var input = document.getElementById("textInput").value;
 	var selectOpponentPlayer2 = document.getElementById("selectOpponentPlayer2").value;
 	var player1Score = document.getElementById("player1Score").value;
 	var player2Score = document.getElementById("player2Score").value;
@@ -74,7 +181,6 @@ $(document).ready(function() {
 	
 	var MatchScore = Parse.Object.extend("MatchScore");
     var matchScore = new MatchScore();
-      //matchScore.save({Player1ID: "1", Player2ID: "2", P1score: "3". P2Score: "1", victor:"3"}, {
 	  matchScore.save({Player1ID: 1, Player2ID: selectOpponentPlayer2, P1score: player1Score, P2Score: player2Score, victor:matchWinner}, {
 		  success: function(object) {
 			alert("Score Successfully Added");
